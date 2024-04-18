ffccb29867612d276a1f0e64dd1c2fa2e2f91607
LiquiyoYT <130622494+LiquiyoYT@users.noreply.github.com>
2024-04-17 20:33:22 -0500

    Update App.js

--- App.js
+++ App.js
@@ -12,6 +12,7 @@ export default class App extends React.Component{
 }  
  
 const switchNavigator = createSwitchNavigator({
+  WelcomeScreen: {Screen:WelcomeScreen,}
   AppTabNavigator: { screen: AppTabNavigator },    
 });
   
